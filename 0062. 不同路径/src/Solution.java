0a5ee3a27aad70a058961c2a469bc97512d3d616
陈庆生 <qingsheng.chen@hand-china.com>
2020-12-09 14:57:43 +0800

    [0062]

---
+++ 0062. 不同路径/src/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * @author 废柴 2020/12/9 14:29
+ */
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            f[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            f[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
+            }
+        }
+        return f[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(new Solution().uniquePaths(3, 7));
+        System.out.println(new Solution().uniquePaths(51, 9));
+    }
+}
